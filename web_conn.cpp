710da23ef6cd9bbd92a6792dd740c0f86b9e1d9c
Dr. Lin, Chao <linchao@OrangePi-WinPlus-2.home>
2018-05-30 21:08:35 +0100

    Start writing code for parsing command line arguments. Finished repeat number argument parsing.
    	modified:   web_conn.cpp
    	modified:   web_conn.h

--- web_conn.cpp
+++ web_conn.cpp
@@ -23,21 +23,67 @@
 
 int main(int argc, char** argv)
 {
-	int repeat = 0;
-	int nthread = 1;
+	bool test_repeat_set = false;	///< Mark whether test repeat number has been set or not. 
+	bool test_threads_set = false;	///< Mark whether test thread count has been set or not. 
+	int test_repeat = 0;			///< Test repeat number (must be >=1) 
+	int test_threads = 1;			///< Test thread number (must be >=1)
 	int i;
 
 	// Set repeat count to 1 if no command line argument is provided. 
 	if(argc == 1)
 	{
+		test_repeat = 1;
+		test_threads = 1;
 	}
-	// Parse arguments
+	// Parse command line arguments
+	// Since the command line arguments syntax is simple and this code
+	// suppose to be easy to move to other platform, this program parses
+	// all command line arguments by itself. 
 	else
 	{
 		for(i=1; i<argc; i++)
 		{
 			if(!strncmp(argv[i], "-n", MAX_STR_LENGTH))
 			{
+				if( test_repeat_set == true)
+				{
+					std::cout << "Error: Can not set test repeat number more than once!" << std::endl;
+					return ERROR_WRONG_ARGS;
+				}
+
+				i++;
+				if(i < argc)
+				{
+					long lnum;
+					char *end;
+					lnum = strtol(argv[i], &end, 10);
+
+					if (argv[i] == end)
+					{
+						std::cout << "Error: wrong value for test repeat number." << std::endl;
+						return ERROR_OUT_OF_RANGE;
+					}
+					else if( (lnum == LONG_MAX) || (lnum == LONG_MIN) || errno == ERANGE)
+					{
+						std::cout << "Error: test repeat number is out of range." << std::endl;
+						return ERROR_OUT_OF_RANGE;
+					}
+					else if( (lnum > MAX_REPEAT) || (lnum < 1))
+					{
+						std::cout << "Error: test repeat number must be in the range of [" << 1 << "," << MAX_REPEAT << "]" << std::endl;
+						return ERROR_OUT_OF_RANGE;
+					}
+					else
+					{
+						test_repeat = (int)lnum;
+						test_repeat_set = true;
+					}
+				}
+				else
+				{
+					std::cout << "Expecting test repeat number (integer)" << std::endl;
+					return ERROR_WRONG_ARGS;
+				}
 			}
 			else if(!strncmp(argv[i], "-H", MAX_STR_LENGTH))
 			{
--- web_conn.h
+++ web_conn.h
@@ -12,11 +12,25 @@
 // Define maximum string length for string compare, copy functions.
 #define MAX_STR_LENGTH		1024
 
-
+// Define my error code
 #define ERROR_WRONG_ARGS	1
 #define ERROR_OUT_OF_RANGE	2
 #define ERROR_TEST_FAILED	3
 #define ERROR_CONN_FAILED	4
+
+// Define the maximum threads this program allows
+//
+// By using multi-threading technology, the program performance could be imporoved in most case. 
+// Consider running this program on a multi-core PC and/or with relatively slow
+// network connection speed, multi-threading can reduce the total testing time dramatically. 
+//
+// However, consider system resoruce usage and thread conflications, number of threads must
+// be considered. Here is a temporary setting which allows up to four threads running on 
+// a quad-core ARM single board computer. 
+#define MAX_THREADS			4
+
+// Set a maximum repeat number to prevent user input a very large repeat number accidently.
+#define MAX_REPEAT			10000
 
 #endif 
 
